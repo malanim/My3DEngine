cdcb9ae1ca37dc4d65ff241b1f008cfa3168455b
malanim <max1129@mail.ru>
2024-11-20 08:48:38 +0300

    render complite

--- vector.py
+++ vector.py
@@ -17,8 +17,7 @@ class Vector3:
 
     def __mul__(self, other):
         if isinstance(other, Matrix4):  # Умножение на матрицу
-            # Создаем вектор в виде массива для умножения
-            vec = np.array([self.x, self.y, self.z, 1])  # Добавляем 1 для однородных координат
+            vec = np.array([self.x, self.y, self.z, 1])  # Однородные координаты
             result = np.dot(other.values, vec)  # Умножаем матрицу на вектор
             return Vector3(result[0], result[1], result[2])  # Возвращаем новый вектор
         elif isinstance(other, (int, float)):  # Умножение на скаляр
@@ -41,6 +40,7 @@ class Vector3:
     def __repr__(self):
         return f"Vector3({self.x}, {self.y}, {self.z})"
 
+
 class Matrix4:
     def __init__(self, values=None):
         if values is None:
@@ -51,6 +51,11 @@ class Matrix4:
     def __mul__(self, other):
         if isinstance(other, Matrix4):
             return Matrix4(np.dot(self.values, other.values))
+        elif isinstance(other, Vector3):
+            # Умножение матрицы на вектор
+            vec = np.array([other.x, other.y, other.z, 1])  # Преобразуем вектор в однородные координаты
+            result = np.dot(self.values, vec)  # Умножаем матрицу на вектор
+            return Vector3(result[0], result[1], result[2])  # Возвращаем новый вектор
         else:
             raise TypeError("Unsupported operand type(s) for *: 'Matrix4' and '{}'".format(type(other).__name__))
 
@@ -100,35 +105,10 @@ class Matrix4:
     def scaling(sx, sy, sz):
         return Matrix4([
             [sx, 0, 0, 0],
-            [0, sy, 0, 0],
+            [0, sy , 0, 0],
             [0, 0, sz, 0],
             [0, 0, 0, 1]
         ])
 
     def __repr__(self):
         return f"Matrix4({self.values})"
-
-# Тестирование vector.py
-# if __name__ == "__main__":
-#     # Тестирование Vector3
-#     v1 = Vector3(1, 2, 3)
-#     v2 = Vector3(4, 5, 6)
-#     print("v1 + v2 =", v1 + v2)
-#     print("v1 - v2 =", v1 - v2)
-#     print("v1 * 2 =", v1 * 2)
-#     print("Длина v1 =", v1.length())
-#     print("Нормализованный v1 =", v1.normalize())
-
-#     # Тестирование Matrix4
-#     m1 = Matrix4.translation(1, 2, 3)
-#     m2 = Matrix4.rotation_x(math.pi / 4)
-#     m3 = m1 * m2
-#     print("Результат умножения матриц =", m3)
-
-# Результаты тестов:
-# v1 + v2 = Vector3(5, 7, 9)
-# v1 - v2 = Vector3(-3, -3, -3)
-# v1 * 2 = Vector3(2, 4, 6)
-# Длина v1 = 3.7416573867739413
-# Нормализованный v1 = Vector3(0.2672612419124244, 0.5345224838248488, 0.8017837257372732)
-# Результат умножения матриц = Matrix4([[1, 0.0, 0.0, 1], [0, 0.7071067811865476, -0.7071067811865476, 2], [0, 0.7071067811865476, 0.7071067811865476, 3], [0, 0.0, 0.0, 1]])
